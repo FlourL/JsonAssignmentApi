33ec52f3eb9681dafbce00a7de09170a62bc7af3
Tom Fjällström <tom.fjallstrom@gmail.com>
2020-01-12 22:57:31 +0100

    Added Dependencies and started with Parser

--- JsonApiAssignment/src/com/example/APIMovie/JsonParser.java
+++ JsonApiAssignment/src/com/example/APIMovie/JsonParser.java
@@ -1,5 +1,39 @@
 package com.example.APIMovie;
 
-public class JsonParser {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.json.JSONObject;
+
+public class JsonParser {
+	
+	final String url = "https://api.themoviedb.org/3/movie/";
+	
+	public String parseFor(String tag) {
+		//JSONObject json = new JSONObject(readUrl("..."));
+		return "";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private String getApiKey() {
+		try {
+			byte[] apiEncoded = Files.readAllBytes(Paths.get(""));
+			return new String(apiEncoded, "UTF-8");
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
